8d608b2dbe0ad346efd15be28dbe439445f1e568
Augusto Moscardo <augustomsaluz@gmail.com>
2025-04-15 23:48:44 -0300

    started observability module

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -1,8 +1,9 @@
 import type { FastifyInstance } from "fastify";
+import { log } from "../infra/logger";
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get('/health', async (request, reply) => {
-
+    log.error('Acessei a rota /health e deu certo!')
     await reply.status(200).send({ message: 'OK!' })
   })
 }
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { fastifyCors } from '@fastify/cors'
 import { uploadImageRoute } from './routes/upload-image'
 import { healthCheckRoute } from './routes/health-check'
 import { fastifyMultipart } from '@fastify/multipart'
+import { log } from './infra/logger'
 
 const server = fastify()
 
@@ -16,4 +17,5 @@ server.register(healthCheckRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
+  log.error('HTTP server running!')
 })

9d75a2f021795437db369d02219c4702eb8d843a
Augusto Moscardo <augustomsaluz@gmail.com>
2025-08-26 23:19:01 -0300

    add @aws-sdk/client-ssm

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ import { healthCheckRoute } from './routes/health-check'
 import { fastifyMultipart } from '@fastify/multipart'
 import { log } from './infra/logger'
 import { vault } from './infra/secret'
+import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm'
+
+const client = new SSMClient({ region: 'us-east-1' })
 
 const server = fastify()
 
@@ -17,8 +20,11 @@ server.register(uploadImageRoute)
 server.register(healthCheckRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(async () => {
-  const values = await vault.read('secret/data/widget-server-stg')
-  console.log(values.data.data);
+  // const values = await vault.read('secret/data/widget-server-stg')
+  const values = await client.send(new GetParameterCommand({
+    Name: 'test'
+  }))
+  console.log(values);
   
   console.log('HTTP server running!')  
   log.info('HTTP server running!')

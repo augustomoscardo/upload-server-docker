402cc2bade1a7794ddd509597e0bd876498efa62
Augusto Moscardo <augustomsaluz@gmail.com>
2025-09-09 16:27:48 -0300

    ajuste para atualizar container no dockerhub

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,13 @@ import { fastifyCors } from '@fastify/cors'
 import { uploadImageRoute } from './routes/upload-image'
 import { healthCheckRoute } from './routes/health-check'
 import { fastifyMultipart } from '@fastify/multipart'
-import { log } from './infra/logger'
-import { vault } from './infra/secret'
-import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm'
+// import { log } from './infra/logger'
+// import { vault } from './infra/secret'
+// import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm'
+// import { kmsValues, secretManagerValues, ssmValues } from './infra/aws'
 
-const client = new SSMClient({ region: 'us-east-1' })
+// const client = new SSMClient({ region: 'us-east-1' })
 
-import { kmsValues, secretManagerValues, ssmValues } from './infra/aws'
 
 const server = fastify()
 
@@ -31,9 +31,9 @@ server.listen({ port: 3333, host: '0.0.0.0' }).then(async () => {
   // const results = await kmsValues()
   // console.log(results);
 
-  const secrets = await secretManagerValues()
-  console.log(secrets);
+  // const secrets = await secretManagerValues()
+  // console.log(secrets);
 
   console.log('HTTP server running!')
-  log.info('HTTP server running!')
+  // log.info('HTTP server running!')
 })
